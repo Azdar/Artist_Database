2c7185db114f4506886719dc5c9d837fa8a77498
Azdar <adam.stogsdill@gmail.com>
2019-05-02 02:47:53 -0600

    Update QueryDataManagment.java

--- QueryDataManagment.java
+++ QueryDataManagment.java
@@ -1,99 +1,80 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
 /**
+ *
  * @author Adam Stogsdill
  *
  */
-public class QueryConversionData {
-
-    private int[] conversion_array;
-    private ResultSet information;
-    private String[] column_names;
-    private ArrayList<Object[]> query_hold = new ArrayList<Object[]>();
-    private ArrayList<Object> list = new ArrayList<Object>();
+public class QueryDataManagment {
 
 
-    public QueryConversionData(ResultSet a) throws SQLException {
-        conversion_array = new int[a.getMetaData().getColumnCount()];
-        column_names = new String[a.getMetaData().getColumnCount()];
-        information = a;
-        this.automaticParse(a);
+    private ArrayList<QueryConversionData> data = new ArrayList<QueryConversionData>();
+    private ArrayList<String> queryHistory = new ArrayList<String>();
+
+
+    /**
+     * CreateQuery is a method that adds a Query to the QueryDataManagment "data" ArrayList which holds all the
+     * QueryConversionData Classes. Then also adds the Query to a "queryHistory" ArrayList which can be called later
+     * in if the user chooses to use one of the CallQuery methods.
+     * @param rs, required ResultSet that will be called and created into a QueryConversionData Class
+     * @param Query, String that is added to the "queryHistory" ArrayList for future use
+     * @throws SQLException
+     */
+    public void createQuery(ResultSet rs, String Query) throws SQLException {
+        this.data.add(new QueryConversionData(rs));
+        this.queryHistory.add(Query);
     }
 
 
     /**
-     * AutomaticParse is a method that takes in the ResultSet from the Query and creates the
-     * conversion array and outputs information alongside the process. First it prints the MetaData
-     * information of each column of the query. Then sends this to a method to automatically output the
-     * information into the console.
-     * @param a
+     * CallQuery is a method that takes in an integer from the "data" ArrayList directly which means that this method
+     * is faster than others in its family and is useful when the user knows the array index of the query that they want
+     * to call once more.
+     * @param a, index of the Query that you want from the "data" ArrayList
      * @throws SQLException
      */
-    public void automaticParse(ResultSet a) throws SQLException {
-        int num_of_columns = a.getMetaData().getColumnCount();
-        System.out.println("Number_Of_Columns: " + num_of_columns);
-
-        int[] conversion = new int[num_of_columns];
-        for (int x = 1; x <= num_of_columns; x++) {
-            conversion[x - 1] = a.getMetaData().getColumnType(x);
-            column_names[x-1] = a.getMetaData().getColumnName(x);
-            System.out.println(a.getMetaData().getColumnName(x) + "  TYPE: " + a.getMetaData().getColumnType(x));
-        }
-        //System.out.println();
-        if(num_of_columns == 1){
-            while(a.next()){
-                list.add(a.getString(1));
-            }
-        }
-        else { OUPUTQUERY(conversion, a); }
+    public void callQuery(int a) throws SQLException{
+        data.get(a).OUPUTQUERY(data.get(a).getConversionArray(), data.get(a).getResultSet());
     }
+
 
     /**
-     * OUTPUTQUERY is a method that automatically outputs the query to the console
-     * @param conversion_array, array that is required to be parsed to know how to convert data.
-     * @param a, ResultSet that is pertaining to the object
+     * CallQuery is a method that takes in a string from the "queryHistory" ArrayList and executed the query that we basically
+     * have cached without having the user call the server for the information again.
+     * @param query, the Query that was used to
      * @throws SQLException
      */
-    public void OUPUTQUERY(int[] conversion_array, ResultSet a) throws SQLException {
-        String result = "";
-        this.conversion_array = conversion_array;
-        while (a.next()) {
-            Object[] obj = new Object[conversion_array.length];
-            for (int i = 0; i < conversion_array.length; i++) {
-                switch(conversion_array[i]) {
-                    case 12:
-                        String string = a.getString(i+1);
-                        obj[i] = string;
-                        result += string + " ";
-                        break;
-                    case 4:
-                        int integer = a.getInt(i+1);
-                        obj[i] = integer;
-                        result += integer + " ";
-                        break;
-                }
-            }
-            this.query_hold.add(obj);
-            result += "\n";
+    public void callQuery(String query) throws SQLException {
+        for(int i = 0; i < queryHistory.size(); i++) {
+            if(queryHistory.get(i).equalsIgnoreCase(query))
+                data.get(i).OUPUTQUERY(data.get(i).getConversionArray(), data.get(i).getResultSet());
         }
-        //System.out.println(result);
     }
 
 
+    public QueryConversionData getQuery(int a) throws SQLException{
+        return data.get(a);
+    }
 
 
-    public int[]     getConversionArray() 		{ return this.conversion_array; }
-    public ResultSet getResultSet() 	  		{ return this.information;      }
-    public String[]  getColumnName()      		{ return this.column_names;     }
-    public ArrayList<Object[]> getObjectArray()	{ return this.query_hold;		}
-    public Object[] getObjectList()             { return this.list.toArray();   }
+    public QueryConversionData getQuery(String query) throws SQLException {
+        for(int i = 0; i < queryHistory.size(); i++) {
+            if(queryHistory.get(i).equalsIgnoreCase(query))
+                return data.get(i);
+        }
+        return null;
+    }
 
+    public int locationInHistory(String q){
+        for(int i = 0; i < queryHistory.size(); i++){
+            if(queryHistory.get(i).equals(q))
+                return i;
+        }
+        return -1;
+    }
 
-
-
-
-
-
+    public int getSize(){
+        return this.data.size() - 1;
+    }
 }
